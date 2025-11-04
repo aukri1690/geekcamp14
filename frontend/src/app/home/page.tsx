a9eff14e58c71aba73ef5902da611597e551340f
aukri1690 <toro369@icloud.com>
2025-11-04 12:03:38 +0900

    feat: implement framework of home full

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,8 +1,11 @@
+import HomeFull from '@/components/HomeFull';
 import HomeEmpty from '@/components/HomeEmpty';
 
 const Home = () => {
   return(
     <>
+      {/* 自己紹介カードの有無で条件分岐する */}
+      {/* <HomeFull /> */}
       <HomeEmpty />
     </>
   );
---
+++ frontend/src/components/HomeFull.tsx
@@ -0,0 +1,9 @@
+const HomeFull = () => {
+  return(
+    <>
+    <h1>自己紹介カードが作成されている場合のホーム画面</h1>
+    </>
+  );
+};
+
+export default HomeFull;
